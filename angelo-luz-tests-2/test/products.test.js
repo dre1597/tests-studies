595c707c3991dede474b09ad94baa3cf205dc131
André Pereira <andrepereira97@yahoo.com>
2022-06-04 09:50:32 -0300

    :white_check_mark: add create product test

--- angelo-luz-tests-2/test/products.test.js
+++ angelo-luz-tests-2/test/products.test.js
@@ -1,6 +1,34 @@
 /* eslint-disable no-undef */
-import request from 'supertest';
+import request from "supertest";
 
-import app from '../src/app';
+import app from "../src/app";
 
 let products;
+
+beforeEach(() => {
+  products = [
+    {
+      code: 1,
+      description: "Product 1",
+      buyPrice: 4000,
+      sellPrice: 8000,
+      tags: ["tag1", "tag2"],
+    },
+    {
+      code: 2,
+      description: "Product 2",
+      buyPrice: 2000,
+      sellPrice: 4000,
+      tags: ["tag2", "tag3"],
+    },
+  ];
+});
+
+it("Should be possible add a new product", async () => {
+  const response = await request(app).post("/products").send(products[0]);
+
+  expect(response.body).toMatchObject({
+    ...products[0],
+    lovers: 0,
+  });
+});
