c5f004fcf307c33694d3dfbd8fe5fbc58fa38f6e
André Pereira <andrepereira97@yahoo.com>
2022-06-05 18:03:32 -0300

    :sparkles: add max length validation on product description

--- angelo-luz-tests-2/src/utils/Validator.js
+++ angelo-luz-tests-2/src/utils/Validator.js
@@ -1,7 +1,7 @@
 export class Validator {
   static validProduct(product) {
     const { description } = product;
-    if (description.length < 3) {
+    if (description.length < 3 || description.length > 50) {
       throw new Error("Description should have between 3 and 50 characters");
     }
 
